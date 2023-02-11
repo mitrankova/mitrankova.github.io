59038b059b93ec64aa2bf9024de934b6843a0222
yoren <mitrankovy@gmail.com>
2023-02-11 23:07:02 +0300

    Update 2

--- js/collapsible.js
+++ js/collapsible.js
@@ -1,8 +1,23 @@
-var coll = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName("collapsible"||"collapsiblepub");
 var i;
 
 for (i = 0; i < coll.length; i++) {
    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+         content.style.display = "none";
+      } else {
+         content.style.display = "block";
+      }
+   });
+}
+
+var collpub = document.getElementsByClassName("collapsiblepub");
+var j;
+
+for (j = 0; j < collpub.length; j++) {
+   collpub[j].addEventListener("click", function() {
       this.classList.toggle("active");
       var content = this.nextElementSibling;
       if (content.style.display === "block") {
